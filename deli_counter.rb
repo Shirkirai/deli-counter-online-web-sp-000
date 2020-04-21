56cf93427cda8f2a8cdba01c424183c99086cbdf
Jung Lee <lee.jung.s95@gmail.com>
2020-04-21 01:34:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,10 @@ def line(customer_list)
   if customer_list.length == 0
     puts "The line is currently empty."
   elsif customer_list.length > 0
+    the_list = []
     counter = 1
     customer_list.each do |customers|
-      puts "The line is currently: #{counter}."
+      the_list << puts "The line is currently: #{counter}."
       counter += 1
     end
   end
