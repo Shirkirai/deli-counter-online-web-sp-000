ca8e4d6fc593d5539433dff4daf371855fb09a8d
Jung Lee <lee.jung.s95@gmail.com>
2020-04-21 01:44:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(customer_list)
     the_list = []
     counter = 1
     customer_list.each do |customers|
-      the_list.shift("#{counter}.to_i. #{customers}")
+      the_list.push("#{counter}.to_i. #{customers}")
       counter += 1
     end
     puts "The line is currently: #{the_list}"
