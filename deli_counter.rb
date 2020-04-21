fc80140982d1758a226eadd4ac57e602b5c154fa
Jung Lee <lee.jung.s95@gmail.com>
2020-04-21 02:20:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ end
 
 def now_serving(array)
   if array.length > 0
-    puts "Currently serving #{array[0]}"
+    puts "Currently serving #{array[0].}"
     array.shift
   else
     puts "There is nobody waiting to be served!"
